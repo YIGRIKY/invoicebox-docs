a37240cd64dc42d1cf022fb6c4dd113efe796b12
Vladimir Lisin <vladimir.lisin@invbox.ru>
2022-02-04 16:59:37 +0300

    Cleanup

--- compile/pandoc_postcompile.py
+++ compile/pandoc_postcompile.py
@@ -14,10 +14,7 @@ print( 'Number of paragraphs: ', paragraphs.count)
 
 document.Paragraphs.Add()
 title = document.Paragraphs(1) 
-title.Range.Text = '«Инвойсбокс» API v3'
-
-smiles = re.compile(r"/\:*\:/")
-controls = re.compile(r"/\{\:*\}/")
+title.Range.Text = '«Инвойсбокс» API v3\n'
 
 for section in sections:
     headersCollection = section.Headers
@@ -25,14 +22,7 @@ for section in sections:
         header.Range.Text = header.Range.Text + "«Инвойсбокс» API v3 (актуально на " + datetime.today().strftime('%d.%m.%Y') + ")"
         print(header.Range.Text)
 
-for paragraph in paragraphs:
-#    print(re.sub("/\:*\:/", "", paragraph.Range.Text))
-    Text = re.sub("Читать далее »", "", paragraph.Range.Text)
-#    paragraph.Range.Text = Text
-    print(Text)
-
-#    paragraph.Range.Text = controls.sub("", paragraph.Range.Text)
-#    print(paragraph.Range.Text)
-
-#paragraph.Range.Text = 'new text containing ocean'
-
+#    for paragraph in section.Range.Paragraphs:
+#        Text = paragraph.Range.Text
+#        Text = re.sub("Читать далее »", "", Text)
+#        paragraph.Range.Text = Text
