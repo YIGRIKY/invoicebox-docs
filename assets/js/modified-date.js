e013b5b83908e6323cdb3b2088f383c290047266
Vladimir Lisin <vladimir.lisin@invbox.ru>
2022-02-10 12:58:18 +0300

    Last modified date improvement

---
+++ assets/js/modified-date.js
@@ -0,0 +1,21 @@
+function setModifiedDate(url,date)
+{
+  if (document.getElementById('last-modified')) {
+    fetch("https://api.github.com/repos/"+url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((commits) => {
+        console.log(commits);
+        var modifiedText = "история изменений";
+        if ( typeof commits != "undefined" && Array.isArray(commits) )
+        {
+           var modified = commits[0]['commit']['committer']['date'].slice(0,10);
+           if(modified != date) {
+              modifiedText = "обновлено: " + modified;
+            }; //
+        }; //
+        document.getElementById('last-modified').textContent = "<a href='https://github.com/"+url+"'>" + modifiedText + "</a>";
+      });
+  }
+};
