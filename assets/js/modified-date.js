02670f1e4c749429016c2a02a69753705570b91e
Vladimir <vladimir.lisin@invbox.ru>
2023-10-21 18:29:37 +0300

    Date format fix

--- assets/js/modified-date.js
+++ assets/js/modified-date.js
@@ -8,11 +8,11 @@ function setModifiedDate(url,date)
       .then((commits) => {
         console.log(commits);
         var modifiedText = "история изменений";
-        if ( typeof commits != "undefined" && Array.isArray(commits) )
+        if (typeof commits != "undefined" && Array.isArray(commits))
         {
            var modified = commits[0]['commit']['committer']['date'].slice(0,10);
            if(modified != date) {
-              modifiedText = "обновлено: " + modified;
+              modifiedText = "обновлено: " + modified.split("-").reverse().join(".");
             }; //
         }; //
         document.getElementById('last-modified').innerHTML = "<a target='_blank' href='https://github.com/"+url+"'>" + modifiedText + "</a>";
